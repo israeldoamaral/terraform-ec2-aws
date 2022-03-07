c1dadf9750a488b274da2c23166b95c02e27df07
israeldoamaral <80478844+israeldoamaral@users.noreply.github.com>
2022-03-07 16:28:22 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "ec2" {
+  count                       = var.ec2_count
+  ami                         = var.ami_id
+  associate_public_ip_address = true
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  # security_groups = [var.security_group]
+  vpc_security_group_ids      = [var.security_group]
+  key_name                    = var.key_name
+  user_data = "${file("${var.userdata}")}"
+
+  tags = {
+    "Name" = "Jenkins"
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "ec2_count" {
+  default = "1"
+}
+
+variable "ami_id" {}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "subnet_id" {}
+
+variable "security_group" {
+  type = string
+}
+
+variable "key_name" {
+
+}
+
+variable "userdata" {}
