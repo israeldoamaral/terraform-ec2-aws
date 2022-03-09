50990943153b2ee557f0d90f461e67479d1f1f0c
israeldoamaral <80478844+israeldoamaral@users.noreply.github.com>
2022-03-08 22:04:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  count                       = var.ec2_count
+  #count                       = var.ec2_count
   ami                         = var.ami_id
   associate_public_ip_address = true
   instance_type               = var.instance_type
